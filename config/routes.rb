98fe5145d3596bf32d5117dd6e45d9f065e38d83
Tomas Stanko <tomas.stanko1000@gmail.com>
2014-11-30 19:01:31 +0100

    objednavky 1

--- config/routes.rb
+++ config/routes.rb
@@ -11,6 +11,8 @@ Depot::Application.routes.draw do
   get "store/index"
   resources :products
 
+  resources :orders
+
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
 
